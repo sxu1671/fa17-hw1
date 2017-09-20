091628a5640b366a20f076b3d1782d6935b8bc0f
sxu1671 <sxu1671@gmail.com>
2017-09-20 14:36:57 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+  	
+  	# array of integers as strings => changes each string into int
+    # add 2 to each number
+    # keep even numbers only
+    # remove duplicates
+    # rejects resulting number > 10
+    # return sum of resultingarray
+    a.map!{|i| i.to_i}.map!{|i| i += 2}.select{|i| i if i % 2 == 0}.uniq.select{|i| i if i <= 10}.sum
   end
 end
 
